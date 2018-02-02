05502ab4b95021a0494f8466803890c9ed013ca7
ross-wang <ross.wang@samsung.com>
2018-02-02 11:24:35 -0800

    add greenspan service

---
+++ greenspan/src/main/java/com/ross/cloud/greenspan/GreenSpanApp.java
@@ -0,0 +1,14 @@
+package com.ross.cloud.greenspan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ross.wang on 2/2/18 for project greenspan.
+ */
+@SpringBootApplication
+public class GreenSpanApp {
+    public static void main (String... args) {
+        SpringApplication.run(GreenSpanApp.class, args);
+    }
+}
